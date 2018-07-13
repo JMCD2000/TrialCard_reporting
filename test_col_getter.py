06cafd138e7a0ab53d6f45c6f9c135db4644ffcf
Jonathan McD <jonathanmcd2000@gmail.com>
2018-07-13 11:35:33 -0400

    revived project conversion
    changes commit 7/12/2018 3:19PM

--- test_col_getter.py
+++ test_col_getter.py
@@ -1,5 +1,26 @@
-myColumnDict = {'Star': 'star', 'Priority': 'priority', 'Pri': 'priority', 'Safe': 'safety', 'S': 'safety', 's': 'safety'}
-
+# Column to variable reference value
+# Excel column header value : defined variable name
+myColumnDict = { 
+ 'Trial Card #': 'dsp',
+ 'Star': 'star',
+ 'Pri': 'pri',
+ 'Priority': 'pri',
+ 'Safe': 'safe',
+ 'S': 'safe',
+ 's': 'safe',
+ 'Saf': 'safe',
+ 'Scrn': 'scrn',
+ 'Act 1': 'ac1',
+ 'Act 2': 'ac2',
+ 'Status': 'stat',
+ 'Action Taken': 'actkn',
+ 'Date Discovered': 'dt_disc',
+ 'Date Closed': 'dt_close',
+ 'Trial ID': 'trial_ID',
+ 'Event': 'Event',
+ 'Aliases': 'dsp',
+}
+# Hard coded values mapping
 def getColumnMap(cellHead):
     if cellHead == 'Star':
         return 'star' #from tblTSM_TC
@@ -11,18 +32,22 @@ def getColumnMap(cellHead):
         print('column header not in list')
         return 'None'
 
+# get mapping from dictionary myColumnDict
 def getColumnDict(cellHead):
     if myColumnDict.get(cellHead, default=True) == True:
         if myColumnDict.get(cellHead, default=None) == None:
-            return 'not found'
+            def_var_name = 'not found'
         else:
-            return myColumnDict.get(cellHead, default=None)
+            def_var_name = myColumnDict.get(cellHead, default=None)
     else:
         myColumnDict.get(cellHead, default=None)
-        return 'not found'
+        def_var_name = 'not found'
             
    
-
+	return def_var_name
+	
+	
+	
 myColVal = input('enter a column value: ')
 
 print(getColumnMap(myColVal))
